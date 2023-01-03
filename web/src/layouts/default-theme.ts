7da4bdd38e804efd543f60a612aa6a4545f2063e
Dhruv Patel <dpatel@gasbuddy.com>
2023-01-03 23:26:47 +0530

    refactor: leverage index.css for global styles cleaning up default theme

--- web/src/layouts/default-theme.ts
+++ web/src/layouts/default-theme.ts
@@ -1,42 +1,14 @@
 import { MantineThemeOverride } from '@mantine/core'
 
 export default {
-  colorScheme: 'light',
-  colors: {
-    deepBlue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
-    blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
-  },
-  shadows: {
-    md: '1px 1px 3px rgba(0, 0, 0, .25)',
-    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-  },
-  headings: {
-    fontFamily:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
-    sizes: {
-      h1: { fontSize: '2rem' },
-    },
-  },
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+  fontSize: '16px',
   globalStyles: () => ({
-    'html, body': {
-      fontFamily:
-        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
-      backgroundColor: '#E2E8F0',
-      margin: '0 auto',
-    },
-    'html *': {
-      boxSizing: 'border-box',
-    },
     'main, header, footer': {
       display: 'flex',
       alignItems: 'center',
-    },
-    section: {
-      backgroundColor: 'white',
-      borderRadius: '0.25rem',
-      padding: '1rem',
-      boxShadow:
-        '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      padding: '0 1rem',
     },
   }),
 } as MantineThemeOverride
